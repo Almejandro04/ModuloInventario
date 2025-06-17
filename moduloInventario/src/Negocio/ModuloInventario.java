c179e0c0bebe275199f79be53eee2b7f2c1918fb
Yasid Jimenez <138026573+ImYasid@users.noreply.github.com>
2025-06-17 15:00:40 -0500

    :sparkles: menu y dependencias

---
+++ moduloInventario/src/Negocio/ModuloInventario.java
@@ -0,0 +1,12 @@
+package Negocio;
+
+import Vista.JFmenuInventario;
+
+public class ModuloInventario {
+
+    public static void main(String[] args) {
+        JFmenuInventario inicio = new JFmenuInventario();
+        inicio.setVisible(true);
+    }
+    
+}
